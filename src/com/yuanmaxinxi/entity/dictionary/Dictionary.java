6fabd630070579067a5b72209c73ac9c5367c27d
YMD-STU004 <YMD-STU004@YMD-STU004-PC>
2018-12-10 16:18:12 +0800

    chenyingtao

---
+++ src/com/yuanmaxinxi/entity/dictionary/Dictionary.java
@@ -0,0 +1,26 @@
+package com.yuanmaxinxi.entity.dictionary;
+
+public class Dictionary {
+	private int id;
+	private String name;
+	private Long typeId;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Long getTypeId() {
+		return typeId;
+	}
+	public void setTypeId(Long typeId) {
+		this.typeId = typeId;
+	}
+
+}
