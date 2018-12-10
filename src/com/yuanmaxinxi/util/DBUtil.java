1780e574a8d413a6769a82c7e3331d4de9455ad7
wangqiangqiang <764372409@qq.com>
2018-12-10 16:56:18 +0800

    修改数据库四元素修改

--- src/com/yuanmaxinxi/util/DBUtil.java
+++ src/com/yuanmaxinxi/util/DBUtil.java
@@ -14,7 +14,7 @@ public class DBUtil {
 		public static Connection getConn() {
 			try {
 				Class.forName(pp.getProperty("className"));
-				conn=DriverManager.getConnection("jdbc:mysql:///enroll","root","admin");
+				conn=DriverManager.getConnection("jdbc:mysql://39.108.187.254:3306/enroll","root","admin");
 			}catch(Exception e) {
 				e.printStackTrace();
 			}
