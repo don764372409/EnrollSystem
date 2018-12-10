501b0a0667870bce7c1d359c040d7a6d9b2218d5
YDM-STU001 <YDM-STU001@YDM-STU001-PC>
2018-12-10 16:08:48 +0800

    更改编码？

--- src/com/yuanmaxinxi/util/DBUtil.java
+++ src/com/yuanmaxinxi/util/DBUtil.java
@@ -8,11 +8,9 @@ import java.util.Properties;
 public class DBUtil {
 		private static Connection conn;
 		private static Properties pp;
-		//设置properties的设置方法
 		public static void setProperties(Properties pro) {
 			pp=pro;
 		}
-		//设置connnection的get方法
 		public static Connection getConn() {
 			try {
 				Class.forName(pp.getProperty("className"));
