18c2571eec7a8b63535bf0cd6753f598233bf06d
wangqiangqiang <764372409@qq.com>
2018-12-11 10:40:48 +0800

    登录

--- src/com/yuanmaxinxi/service/AdminService.java
+++ src/com/yuanmaxinxi/service/AdminService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import com.yuanmaxinxi.dao.admin.AdminDAO;
 import com.yuanmaxinxi.dto.BaseQueryPageDTO;
+import com.yuanmaxinxi.dto.ResultDTO;
 import com.yuanmaxinxi.entity.admin.Admin;
+import com.yuanmaxinxi.util.StringUtil;
 
 public class AdminService {
 	private AdminDAO adminDAO = new AdminDAO();
@@ -26,6 +28,21 @@ public class AdminService {
 	}
 
 	public List<Admin> queryPage(BaseQueryPageDTO dto) {
+		return null;
+	}
+	/**
+	 * 登录方法
+	 * @param username
+	 * @param password
+	 * @return
+	 */
+	public Admin login(String username, String password) {
+		if (StringUtil.isNullOrEmpty(username)) {
+			throw new RuntimeException("账号不能为空");
+		}
+		if (StringUtil.isNullOrEmpty(password)) {
+			throw new RuntimeException("密码不能为空");
+		}
 		return null;
 	}
 }
