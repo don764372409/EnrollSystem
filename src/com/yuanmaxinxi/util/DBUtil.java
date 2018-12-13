ad7b91cc0e81e950468960a22411b2af394d3787
yingtao666 <891577851@qq.com>
2018-12-13 10:31:51 +0800

    1

--- src/com/yuanmaxinxi/util/DBUtil.java
+++ src/com/yuanmaxinxi/util/DBUtil.java
@@ -1,23 +1,4 @@
-<<<<<<< HEAD
-package com.yuanmaxinxi.util;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-public class DBUtil {
-	private static Connection conn;
-	static {
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			conn=DriverManager.getConnection("jdbc:mysql://39.108.187.254:3306/enroll?useUnicode=true&characterEncoding=UTF-8","root","admin");
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	public static Connection getConn() {
-		return conn;
-	}
-}
-=======
 package com.yuanmaxinxi.util;
 
 import java.sql.Connection;
@@ -36,4 +17,4 @@ public class DBUtil {
 		return conn;
 	}
 }
->>>>>>> branch 'master' of https://github.com/don764372409/EnrollSystem.git
+
