fe3a80d1cf88a2dec7b4ba847bb78190c95eb464
mengyaxi <397815571@qq.com>
2019-01-22 14:53:45 +0800

    职业DAO与service的修改

--- src/com/yuanmaxinxi/dao/occupation/OccupationDAO.java
+++ src/com/yuanmaxinxi/dao/occupation/OccupationDAO.java
@@ -11,102 +11,23 @@ import com.yuanmaxinxi.dto.BaseQueryPageDTO;
 import com.yuanmaxinxi.entity.occupation.Occupation;
 import com.yuanmaxinxi.util.DBUtil;
 
-public class OccupationDAO implements BaseDAO<Occupation>{
+public interface OccupationDAO extends BaseDAO<Occupation>{
 
-	@Override
-	public int insert(Occupation obj) {
-		try {
-			//获取sql语句
-			String sql = "insert into t_occupation(name,pId,remark,workContent)values (?,?,?,?)";
-				PreparedStatement state = DBUtil.getConn().prepareStatement(sql);
-				state.setObject(1, obj.getName());
-				state.setObject(2, obj.getpId());
-				state.setObject(3, obj.getRemark());
-				state.setObject(4, obj.getWorkContent());
-				return state.executeUpdate();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		return -1;
-	}
+	
+	public int insert(Occupation obj);
 
-	@Override
-	public int update(Occupation obj) {
-		try {
-			String sql = "update t_occupation set name =?,pId=?, remark=?,workContent=? where id = ?";
-				PreparedStatement state = DBUtil.getConn().prepareStatement(sql);
-				state.setObject(1, obj.getName());
-				state.setObject(2, obj.getpId());
-				state.setObject(3, obj.getRemark());
-				state.setObject(4, obj.getWorkContent());
-				state.setObject(5, obj.getId());
-				return state.executeUpdate();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		return -1;
-	}
-	@Override
-	public int delete(Long id) {
-		try {
-			String sql = "delete from t_occupation where id =?";
-				PreparedStatement state = DBUtil.getConn().prepareStatement(sql);
-				state.setObject(1, id);
-				return state.executeUpdate();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		return -1;
-	}
+	
+	public int update(Occupation obj);
+	
+	public int delete(Long id);
 
-	@Override
-	public Occupation selectOneById(Long id) {
-		List<Occupation> list = new ArrayList<>();
-		try {
-			String sql = "select * from t_occupation where id =?";
-			PreparedStatement state = DBUtil.getConn().prepareStatement(sql);
-			state.setObject(1, id);
-			ResultSet result = state.executeQuery();
-			if(result.next()) {
-				Occupation oc = new Occupation();
-				oc.setId(result.getLong("id"));
-				oc.setName(result.getString("name"));
-				oc.setpId(result.getLong("pId"));
-				oc.setRemark(result.getString("remark"));
-				oc.setWorkContent(result.getString("workContent"));
-				return oc;
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
+	
+	public Occupation selectOneById(Long id);
 
-	@Override
-	public List<Occupation> selectAll() {
-		List<Occupation> list = new ArrayList<>();
-		try {
-			String sql = "select * from t_occupation";
-			PreparedStatement state = DBUtil.getConn().prepareStatement(sql);
-			ResultSet result = state.executeQuery();
-			while(result.next()) {
-				Occupation op = new Occupation();
-				op.setId(result.getLong("id"));
-				op.setName(result.getString("name"));
-				op.setpId(result.getLong("pId"));
-				op.setRemark(result.getString("remark"));
-				op.setWorkContent(result.getString("workContent"));
-				list.add(op);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return list;
-	}
+	
+	public List<Occupation> selectAll();
 
-	@Override
-	public void queryPage(BaseQueryPageDTO<Occupation> dto) {
-		
-	}
+	
+	public void queryPage(BaseQueryPageDTO<Occupation> dto);
 
 }
--- src/com/yuanmaxinxi/service/OccupationService.java
+++ src/com/yuanmaxinxi/service/OccupationService.java
@@ -2,14 +2,25 @@ package com.yuanmaxinxi.service;
 
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+
+import com.yuanmaxinxi.dao.admin.AdminDAO;
 import com.yuanmaxinxi.dao.occupation.OccupationDAO;
 import com.yuanmaxinxi.dto.BaseQueryPageDTO;
 import com.yuanmaxinxi.entity.admin.Admin;
 //github.com/don764372409/EnrollSystem.git
 import com.yuanmaxinxi.entity.occupation.Occupation;
+import com.yuanmaxinxi.util.DBUtil;
 
 public class OccupationService {
-	private OccupationDAO ocpDAO = new OccupationDAO();
+	private OccupationDAO ocpDAO;
+	private void init() {
+		SqlSession session = DBUtil.openSession();
+		ocpDAO = session.getMapper(OccupationDAO.class);
+	}
+	public OccupationService() {
+		init();
+	}
 	public void insert(Occupation obj) {
 		ocpDAO.insert(obj);
 	}
