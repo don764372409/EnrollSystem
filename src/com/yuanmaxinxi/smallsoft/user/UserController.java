9e8f2e6b50db405acb79d1e249217d5aa6f3a9d0
wangqiangqiang <764372409@qq.com>
2019-05-28 14:29:49 +0800

    123

--- src/com/yuanmaxinxi/smallsoft/user/UserController.java
+++ src/com/yuanmaxinxi/smallsoft/user/UserController.java
@@ -150,11 +150,14 @@ public class UserController{
 		return user;
 	}
 	@RequestMapping("/toAccount")
-	public String toAccount(String openid,Model model) {
+	public String toAccount(Model model,String code) {
+		System.out.println(code);
+		JSONObject doGet = doGet("https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx4567caad1b60b9e1&secret=75f903c2c84c191b7da4ca506ff35e70&code="+code+"&grant_type=authorization_code");
+		System.out.println(doGet);
+		String openid = doGet.getString("openid");
+//		String openid = "oLIw75Gcx0gWb4u4P6Wl3nckpYI4";
 		//先获取Code
-		
 		//获取openID
-
 		//获取用户昵称
 		User user = userService.selectOneByOpenid(openid);
 		//获取剩余积分
@@ -167,52 +170,8 @@ public class UserController{
 		return "account/account";
 	}
 	@RequestMapping("/index")
-	public void getCode(HttpServletRequest req,HttpServletResponse response) {
-			String code = req.getParameter("code");
-			if(StringUtil.isNotNullAndEmpty(code)) {
-				System.out.println("1111111111");
-				System.out.println(code);
-				  try {
-					response.sendRedirect("http://www.methodol-edu.com/SSM/soft/user/toAccount");
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-	        StringBuffer sb = new StringBuffer();
-	        StringBuffer encodeUrl = new StringBuffer(300);
-	        //公众号中配置的回调域名（网页授权回调域名）
-	        String appId ="wx934f1fc99b01220a";
-	        sb.append("https://open.weixin.qq.com/connect/oauth2/authorize?appid=");
-	        sb.append(appId);
-	        String url = "";
-	        try {
-	            //对重定向url进行编码，官方文档要求
-	            url = URLEncoder.encode("https://www.methodol-edu.com/SSM/soft/user/index", "utf-8");
-	            sb.append("&redirect_uri=").append(url);
-	            //网页授权的静默授权snsapi_base
-	            sb.append("&response_type=code&scope=snsapi_base&state=State");
-	            response.sendRedirect(sb.toString());
-	        } catch (Exception e) {
-	        	e.printStackTrace();
-	            throw new RuntimeException("重定向url编码失败");
-	        }
-	}
-	@RequestMapping("/secc")
-	@ResponseBody
-	public void scc(HttpServletRequest req,HttpServletResponse resp) {
-		System.out.println("222222222222222");
-	
-	}
-	@RequestMapping("/get")
-	@ResponseBody
-	public ResultDTO getCode1(HttpServletRequest req,HttpServletResponse resp) {
-		ResultDTO dto;
-		System.out.println("进入到获取code的页面");
-		System.out.println(req.getContextPath());
-	    String code = req.getParameter("code");
-	    System.out.println(code);
-		dto = ResultDTO.putSuccessObj("获取成功!", code);
-		return dto;
+	public String getCode(HttpServletRequest req,HttpServletResponse response) {
+		return "shouquan/index";
 	}
 	@RequestMapping("/validataWx")
 	public void validataWx(String signature,String timestamp,String nonce,String echostr,HttpServletResponse resp) throws IOException {
--- webapp/WEB-INF/shouquan/index.jsp
+++ webapp/WEB-INF/shouquan/index.jsp
@@ -1,58 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
 <%@ page language="java" contentType="text/html; charset=utf-8"
-    pageEncoding="utf-8"%>
+	pageEncoding="utf-8"%>
 <head>
-    <meta charset="UTF-8">
-    <title>微信公众号授权</title>
-    <button onclick="wxLogin(callback)">点击获取code</button>
+<meta charset="UTF-8">
+<title>微信公众号授权</title>
 </head>
-
 <body>
-<script>
-function callback(result) {    
-    alert('success');    
-    alert(result);  //输出openid  
-}
-	function getUrlParam(name) {
-	  var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
-	  console.log(reg);
-	  var r = window.location.search.substr(1).match(reg);
-	  if (r != null) return unescape(r[2]);
-	  return null;
-	}
-	function wxLogin(callback) {
-	 /*  var appId = 'wx934f1fc99b01220a';
-	  var oauth_url = 'http://www.methodol-edu.com/SSM/soft/user/get';
-		 var tt =  location.href.split('#')[0];
-		 callback(tt);
-	  var url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=" + appId + "&redirect_uri=" + location.href.split('#')[0] + "&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect"
-	  var code = getUrlParam("code");
-	  callback(code); */
-	/*   if (!code) {
-	    window.location = url;
-	    
-	  } else { */
-	    $.ajax({
-	      type: 'GET',
-	      url: oauth_url,
-	      dataType: 'json',
-	      data: {
-	        code: code
-	      },
-	      success: function (data) {
-	        if (data.code === 200) {
-	          callback(data.data)
-	        }
-	      },
-	      error: function (error) {
-	        throw new Error(error)
-	      }
-	    })
-	  }
-	}
-	  
-</script>
+	<button onclick="wxLogin()">点击获取code</button>
+	<script>
+		function wxLogin() {
+			var appId = 'wx4567caad1b60b9e1';
+			var oauth_url = 'https://www.methodol-edu.com/SSM/soft/user/toAccount';
+			var url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid="
+					+ appId
+					+ "&redirect_uri="
+					+ encodeURIComponent(oauth_url)
+					+ "&response_type=code&scope=snsapi_base&state=STATE#wechat_redirect";
+			location.href = url;
+		}
+	</script>
 </body>
 
 </html>
